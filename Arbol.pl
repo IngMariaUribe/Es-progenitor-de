dcda04e64fd1e30e21546b7d14116ced4a736e00
YeimerSN <52164044+YeimerSN@users.noreply.github.com>
2020-07-07 19:51:00 -0500

    Update Arbol.pl

--- Arbol.pl
+++ Arbol.pl
@@ -5,15 +5,15 @@ progenitor(pedro,martha).
 progenitor(cecilia,valentina).
 progenitor(pedro,valentina).
 
-
-progenitor(martha, paula).
-progenitor(juan, paula).
-progenitor(martha, yilber).
-progenitor(juan, yilber).
-
 progenitor(jose,patricio).
 progenitor(jose,daniela).
 progenitor(jose,viviana).
 progenitor(daniel,patricio).
 progenitor(daniel,daniela).
 progenitor(daniel,viviana).
+
+
+progenitor(martha, paula).
+progenitor(juan, paula).
+progenitor(martha, yilber).
+progenitor(juan, yilber).
