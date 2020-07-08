ed4d05cee344d5f74e54f411799b8e2cbcfcab5c
YeimerSN <52164044+YeimerSN@users.noreply.github.com>
2020-07-08 18:56:40 -0500

    Update Arbol.pl

--- Arbol.pl
+++ Arbol.pl
@@ -18,8 +18,8 @@ progenitor(juan, paula).
 progenitor(martha, yilber).
 progenitor(juan, yilber).
 
-eshijo(X,Y):- progenitor(Y,X).
-casado(X,Y):- eshijo(C,X), eshijo(C,Y).
-feliz(X) :- casado(X,_).
-
-
+eshijo(X,Y):-progenitor(Y,X).casado(X,Y):- eshijo(C,X), eshijo(C,Y).
+hermano(X,Y):-progenitor(C,X),progenitor(C,Y).
+tio(X,Y):-progenitor(Z,Y),hermano(X,Z).
+sobrino(X,Y):-hermano(Z,Y),eshijo(X,Z).
+primo(X,Y):-progenitor(Z,X),progenitor(W,Y),hermano(Z,W).
