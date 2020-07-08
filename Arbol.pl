a89375efd51c1481560ae4996ceb2d7573870cc1
YeimerSN <52164044+YeimerSN@users.noreply.github.com>
2020-07-08 18:58:21 -0500

    Update Arbol.pl

--- Arbol.pl
+++ Arbol.pl
@@ -23,7 +23,7 @@ casado(X,Y):- eshijo(C,X), eshijo(C,Y).
 feliz(X) :- casado(X,_).
 esNieto(A,B):- progenitor(B,C),progenitor(C,A).
 esAbuelo(A,B):- esNieto(B,A).
-esHermano(A,B): - progenitor(C,A),progenitor(C,B).
+esHermano(A,B):- progenitor(C,A),progenitor(C,B).
 tio(X,Y):-progenitor(Z,Y),hermano(X,Z).
 sobrino(X,Y):-hermano(Z,Y),eshijo(X,Z).
 primo(X,Y):-progenitor(Z,X),progenitor(W,Y),hermano(Z,W).
